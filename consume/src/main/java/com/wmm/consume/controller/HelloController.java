9b0ffcbe0d103b4adc22ffe61aa3a79291a8bbcb
wangmm1994 <769669064@qq.com>
2023-02-22 22:20:16 +0800

    test

---
+++ consume/src/main/java/com/wmm/consume/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.wmm.consume.controller;
+
+import com.wmm.consume.inter.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    private HelloRemote helloRemote;
+    @RequestMapping("/hello")
+    public String hello(String name){
+        return "consume:" + helloRemote.hello(name);
+    }
+}
---
+++ consume/src/main/java/com/wmm/consume/inter/HelloRemote.java
@@ -0,0 +1,11 @@
+package com.wmm.consume.inter;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "produce")
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    String hello(@RequestParam(value = "name") String name);
+}
