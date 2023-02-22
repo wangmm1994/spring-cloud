8443b41c0d2c3e15c01b513ba5d26cc798e84966
wangmm1994 <769669064@qq.com>
2023-02-22 22:48:49 +0800

    test

--- consume/src/main/java/com/wmm/consume/inter/HelloRemote.java
+++ consume/src/main/java/com/wmm/consume/inter/HelloRemote.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name= "produce")
+@FeignClient(name= "produce",fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
     @RequestMapping(value = "/hello")
     String hello(@RequestParam(value = "name") String name);
