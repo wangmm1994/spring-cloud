69d46ebac605161c62b961a64fe12eb0ea98d2ad
wangmm1994 <769669064@qq.com>
2025-06-28 09:58:25 +0800

    Update HelloController.java

--- consume/src/main/java/com/wmm/consume/controller/HelloController.java
+++ consume/src/main/java/com/wmm/consume/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     private HelloRemote helloRemote;
     @RequestMapping("/hello")
     public String hello(String name){
-        return "con111q1111111.e:" + helloRemote.hello(name);
+        return "con111q1111111111.e:" + helloRemote.hello(name);
     }
 }
