9c39b9b0763b89f8254e1d8ea41d3cd563eb7ea2
wangmm1994 <769669064@qq.com>
2025-12-13 16:43:18 +0800

    Update response prefix in HelloController

--- consume/src/main/java/com/wmm/consume/controller/HelloController.java
+++ consume/src/main/java/com/wmm/consume/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     private HelloRemote helloRemote;
     @RequestMapping("/hello")
     public String hello(String name){
-        return "co11112e:" + helloRemote.hello(name);
+        return "co112e:" + helloRemote.hello(name);
     }
 }
